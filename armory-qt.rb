0438cab7b7ebfb9ff7ba5fef9e5238a6b9a506e3
Bryan Stitt <bryan@stitthappens.com>
2012-12-03 17:49:47 -0800

    armory-qt 0.85-beta

--- armory-qt.rb
+++ armory-qt.rb
@@ -2,7 +2,10 @@ require 'formula'
 
 class ArmoryQt < Formula
   homepage 'http://bitcoinarmory.com/'
-  head 'https://github.com/WyseNynja/BitcoinArmory/tarball/dev'
+  head 'https://github.com/etotheipi/BitcoinArmory/archive/master.tar.gz'
+  url 'https://github.com/etotheipi/BitcoinArmory/archive/v0.85-beta.tar.gz'
+  sha1 '26354bdb5096ea4a9aeccbf1c80e103fb89c72a1'
+
   depends_on 'cryptopp'
   depends_on 'swig'
   depends_on 'sip'
@@ -10,24 +13,40 @@ class ArmoryQt < Formula
   depends_on 'pyqt'
   depends_on 'twisted' => :python
 
+  def patches
+    DATA
+  end
+
   def install
     ENV.j1  # if your formula's build system can't parallelize
 
+    # my makefile patches weren't working
     system "make"
-    system "make DESTDIR=#{prefix} install"
+    system "mkdir -p #{prefix}/share/armory/img"
+    system "cp *.py *.so README LICENSE #{prefix}/share/armory/"
+    system "cp img/* #{prefix}/share/armory/img"
     bin.install 'ArmoryQt.command'
   end
 
-  def test
-    system "PYTHONPATH=`brew --prefix`/lib/python2.7/site-packages /usr/bin/python `brew --prefix bitcoinarmory`/share/armory/ArmoryQt.py -h"
-  end
-
   def caveats; <<-EOS.undent
-    BitcoinArmory.command was installed in
+    ArmoryQt.command was installed in
         #{bin}
 
     To symlink into ~/Applications, you can do:
-        ln -s #{bin}/ArmoryQt.command ~/Applications
+        ln -s #{bin}/ArmoryQt.command ~/Applications/ArmoryQt
+
+    Or you can just run 'ArmoryQt.command' from your terminal
     EOS
   end
 end
+
+__END__
+diff --git a/ArmoryQt.command b/ArmoryQt.command
+new file mode 100644
+index 0000000..2cc2154
+--- /dev/null
++++ b/ArmoryQt.command
+@@ -0,0 +1,3 @@
++#!/bin/sh
++PYTHONPATH=`brew --prefix`/lib/python2.7/site-packages /usr/bin/python `brew --prefix`/share/armory/ArmoryQt.py
++
