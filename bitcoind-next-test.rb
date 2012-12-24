067702d9ec83b60291c8bea5ea422eb6599cac07
Bryan Stitt <bryan@stitthappens.com>
2012-12-23 16:18:51 -0800

    switch from tar.gz to git

--- bitcoind-next-test.rb
+++ bitcoind-next-test.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class BitcoindNextTest < Formula
   homepage 'http://bitcoin.org/'
-  head 'https://github.com/luke-jr/bitcoin/tarball/next-test'
-  version 'next-test'
+  head 'https://github.com/luke-jr/bitcoin.git', :branch => 'next-test'
+
   depends_on 'boost'
   depends_on 'berkeley-db4'
   depends_on 'miniupnpc' if build.include? 'with-upnp'
@@ -13,35 +13,20 @@ class BitcoindNextTest < Formula
 
   def install
     cd "src" do
-        system "make", "-f", "makefile.osx",
-                       "DEPSDIR=#{HOMEBREW_PREFIX}",
-                       "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
-                       "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
-        system "mv bitcoind bitcoind-next-test"
-        bin.install "bitcoind-next-test"
+      system "make", "-f", "makefile.osx",
+        "DEPSDIR=#{HOMEBREW_PREFIX}",
+        "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
+        "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
+      system "mv bitcoind bitcoind-next-test"
+      bin.install "bitcoind-next-test"
     end
   end
 
   def caveats; <<-EOS.undent
-    You will need to setup your bitcoin.conf:
-        echo "rpcuser=user" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
-        echo "rpcpassword=password" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
+    You will need to setup your bitcoin.conf if you haven't already done so:
+
+    echo "rpcuser=user" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
+    echo "rpcpassword=password" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
     EOS
   end
 end
-
-
-__END__
-diff --git a/src/makefile.osx b/src/makefile.osx
-index 11c6248..112d9f7 100644
---- a/src/makefile.osx
-+++ b/src/makefile.osx
-@@ -7,7 +7,7 @@
- # Originally by Laszlo Hanyecz (solar@heliacal.net)
-
- CXX=llvm-g++
--DEPSDIR=/opt/local
-+DEPSDIR?=/opt/local
-
- INCLUDEPATHS= \
-  -I"$(CURDIR)" \
--- bitcoind.rb
+++ bitcoind.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Bitcoind < Formula
   homepage 'http://bitcoin.org/'
-  head 'https://github.com/bitcoin/bitcoin/archive/master.tar.gz'
-  url 'https://github.com/bitcoin/bitcoin/archive/v0.7.2.tar.gz'
-  sha1 '6afb648f273a52934a65d8a127a08dccdb74db48'
+  head 'https://github.com/bitcoin/bitcoin.git'
+  url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.7.2'
   version '0.7.2'
 
   depends_on 'boost'
@@ -16,35 +15,20 @@ class Bitcoind < Formula
 
   def install
     cd "src" do
-        system "make", "-f", "makefile.osx",
-                       "DEPSDIR=#{HOMEBREW_PREFIX}",
-                       "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
-                       "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
-        system "strip bitcoind"
-        bin.install "bitcoind"
+      system "make", "-f", "makefile.osx",
+        "DEPSDIR=#{HOMEBREW_PREFIX}",
+        "USE_UPNP=#{(build.include? 'with-upnp') ? '1' : '-'}",
+        "USE_IPV6=#{(build.include? 'without-ipv6') ? '-' : '1'}"
+      system "strip bitcoind"
+      bin.install "bitcoind"
     end
   end
 
   def caveats; <<-EOS.undent
-    You will need to setup your bitcoin.conf:
-        echo "rpcuser=user" >> ~/Library/Application Support/Bitcoin/bitcoin.conf
-        echo "rpcpassword=password" >> ~/Library/Application Support/Bitcoin/bitcoin.conf
+    You will need to setup your bitcoin.conf if you haven't already done so:
+
+    echo "rpcuser=user" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
+    echo "rpcpassword=password" >> "~/Library/Application Support/Bitcoin/bitcoin.conf"
     EOS
   end
 end
-
-
-__END__
-diff --git a/src/makefile.osx b/src/makefile.osx
-index 11c6248..112d9f7 100644
---- a/src/makefile.osx
-+++ b/src/makefile.osx
-@@ -7,7 +7,7 @@
- # Originally by Laszlo Hanyecz (solar@heliacal.net)
-
- CXX=llvm-g++
--DEPSDIR=/opt/local
-+DEPSDIR?=/opt/local
-
- INCLUDEPATHS= \
-  -I"$(CURDIR)" \
