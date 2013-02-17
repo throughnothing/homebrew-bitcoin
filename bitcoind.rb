16d41f225393ff2fab87cf998aae2239a6dc3860
Bryan Stitt <bryan@stitthappens.com>
2013-02-16 21:54:51 -0800

    bitcoind v0.8.0rc1 (devel)

--- bitcoind.rb
+++ bitcoind.rb
@@ -6,12 +6,22 @@ class Bitcoind < Formula
   url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.7.2'
   version '0.7.2'
 
+  devel do
+    url 'https://github.com/bitcoin/bitcoin.git', :tag => 'v0.8.0rc1'
+    version 'v0.8.0rc1'
+  end
+
   depends_on 'boost'
   depends_on 'berkeley-db4'
   depends_on 'miniupnpc' if build.include? 'with-upnp'
 
   option 'with-upnp', 'Compile with UPnP support'
   option 'without-ipv6', 'Compile without IPv6 support'
+
+  def patches
+    # fixes berkeley-db4 include and lib path
+    DATA
+  end
 
   def install
     cd "src" do
@@ -32,3 +42,23 @@ class Bitcoind < Formula
     EOS
   end
 end
+__END__
+diff --git a/src/makefile.osx b/src/makefile.osx
+index 359739b..7b2f6e2 100644
+--- a/src/makefile.osx
++++ b/src/makefile.osx
+@@ -13,11 +13,11 @@ INCLUDEPATHS= \
+  -I"$(CURDIR)" \
+  -I"$(CURDIR)"/obj \
+  -I"$(DEPSDIR)/include" \
+- -I"$(DEPSDIR)/include/db48"
++ -I"$(DEPSDIR)/opt/berkeley-db4/include"
+ 
+ LIBPATHS= \
+  -L"$(DEPSDIR)/lib" \
+- -L"$(DEPSDIR)/lib/db48"
++ -L"$(DEPSDIR)/opt/berkeley-db4/lib"
+ 
+ USE_UPNP:=1
+ USE_IPV6:=1
+
